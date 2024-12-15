3e8e266aab39600c55615f6cdb7d7d5bd2c68566
slabwu <134193743+slabwu@users.noreply.github.com>
2024-12-15 19:08:27 +0800

    Create forward and back buttons

--- script.js
+++ script.js
@@ -2,16 +2,25 @@ class ImageCarousel {
     constructor(name) {
         this.container = document.querySelector(`#${name}`);
         this.image = document.querySelector(`#${name} > img`);
+        this.backBtn = document.querySelector(`#${name} > .lastImage`);
+        this.forwardBtn = document.querySelector(`#${name} > .nextImage`);
+
         this.images = ["assets/bachalpsee.jpg", "assets/hang-son-doon.jpg", "assets/matterhorn.jpeg"];
         this.current = 0;
         this.image.src = `${this.images[this.current]}`;
         this.changeImage();
 
-        window.addEventListener("click", () => {this.selectNext()});
+        this.backBtn.addEventListener("click", () => {this.selectPrevious()});
+        this.forwardBtn.addEventListener("click", () => {this.selectNext()});
     }
 
     selectNext() {
-        this.current = ((this.images.length - 1) === this.current)? 0 : this.current + 1;
+        this.current = (this.current === (this.images.length - 1))? 0 : this.current + 1;
+        this.changeImage();
+    }
+
+    selectPrevious() {
+        this.current = (this.current === 0)? this.images.length - 1 : this.current - 1;
         this.changeImage();
     }
 
