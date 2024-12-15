ab05ecbcc241c5d9b32823996d933b8ed0e03c98
slabwu <134193743+slabwu@users.noreply.github.com>
2024-12-15 19:48:20 +0800

    Add active state to paginator

--- script.js
+++ script.js
@@ -14,12 +14,15 @@ class ImageCarousel {
         for (let i = 0; i < this.images.length; i++) {
             let element = document.createElement("div");
             element.dataset.value = i;
+            if (i === 0) element.classList.add("active");
             this.paginator.appendChild(element);
+
             element.addEventListener("click", () => {
                 this.current = element.dataset.value;
                 this.changeImage();
             });
         }
+        this.paginators = Array.from(this.paginator.children);
 
         this.backBtn.addEventListener("click", () => {this.selectPrevious()});
         this.forwardBtn.addEventListener("click", () => {this.selectNext()});
@@ -37,6 +40,19 @@ class ImageCarousel {
 
     changeImage() {
         this.image.src = `${this.images[this.current]}`;
+        this.updatePaginator();
+    }
+
+    updatePaginator() {
+        if (this.paginators) {
+            this.paginators.forEach(paginator => {
+                if (paginator.dataset.value == this.current) {
+                    paginator.classList.add("active");
+                } else {
+                    paginator.classList.remove("active");
+                }
+            })
+        }
     }
 }
 
